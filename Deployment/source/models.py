a4ce786cede96297ad758abe6c3c4304146de664
Juan Francisco <juanfrh7@gmail.com>
2022-01-25 17:42:07 +0000

    import the rest of the notebook

--- Deployment/source/models.py
+++ Deployment/source/models.py
@@ -142,3 +142,167 @@ class models():
         print(classification_report(y_test, y_pred, output_dict=True))
 
         return best_model, classification_report
+
+## Creating the folder
+dir_str = 'Results_' + str(datetime.datetime.now())
+os.mkdir(dir_str)
+
+## Preprocessing
+preprocessed_data, common_empty_columns = preprocessing(df_retired, df_non_retired)
+## Save
+preprocessed_data.to_csv(os.path.join(dir_str, 'preprocessed_data.csv'))
+pickle.dump(common_empty_columns, open(os.path.join(dir_str,'common_empty_columns.pkl'), 'wb'))
+
+
+## Normalization
+scaled_data, scaler = normalize_data(preprocessed_data)
+## Save
+scaled_data.to_csv(os.path.join(dir_str,'scaled_data.csv'))
+pickle.dump(scaler, open(os.path.join(dir_str,'scaler.pkl'), 'wb'))
+
+
+## PCA
+pca_data, pca_ = pca(scaled_data, 250)
+## Save
+pca_data.to_csv(os.path.join(dir_str,'pca_data.csv'))
+pickle.dump(pca_, open(os.path.join(dir_str,'pca_model.pkl'), 'wb'))
+
+##---------------------------------------------------------------------------------------------------------
+## ML SVM
+grid_param_svm = {'C':  [0.0001, 0.001,0.01, 0.1, 1, 10, 100],
+                  'gamma' : ['scale', 'auto', 0.0001, 0.001, 0.01, 0.1, 10, 100],
+                  'class_weight': ['balanced', None],
+                  'kernel' : ['sigmoid','poly','rbf']}
+best_svm, report_svm = svm_churn(pca_data, grid_param_svm)
+## Save
+pickle.dump(best_svm, open(os.path.join(dir_str,'svm.sav'), 'wb'))
+pickle.dump(report_svm, open(os.path.join(dir_str,'svm_metrics.pkl'), 'wb'))
+
+
+##----------------------------------------------------------------------------------------------------------
+## ML Logistic Regression
+grid_param_lr = {"penalty": ["l1", "l2", "elasticnet", "none"],
+              "C": [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+              "solver": ["newton-cg", "lbfgs", "liblinear", 'saga'],
+              "max_iter": [500]
+              }
+best_lr, report_lr = log_reg_churn(pca_data, grid_param_lr)
+## Save
+pickle.dump(best_lr, open(os.path.join(dir_str,'lr.sav'), 'wb'))
+pickle.dump(report_lr, open(os.path.join(dir_str,'lr_metrics.pkl'), 'wb'))
+
+##----------------------------------------------------------------------------------------------------------
+## ML Neural Network
+grid_param_nn = {
+    "activation": ['selu','softplus','softmax'],
+    "init_mode": ['he_normal', 'glorot_normal'],
+    "dropout_rate": [0.8],
+    "units": [[8, 4]],
+    "optimizer": ['RMSprop', 'Adam', 'SGD'],
+    "hiden_layers": [2],
+    "epochs": [15],
+    "batch_size":  [128]
+}
+best_nn, report_nn = nn_churn(pca_data, grid_param_nn)
+## Save
+pickle.dump(best_nn, open(os.path.join(dir_str,'nn.sav'), 'wb'))
+pickle.dump(report_nn, open(os.path.join(dir_str,'nn_metrics.pkl'), 'wb'))
+
+#TTS for SVM
+
+## Creating the folder
+dir_str = 'Results_' + str(datetime.datetime.now())
+os.mkdir(dir_str)
+
+## Preprocessing
+preprocessed_data, common_empty_columns = preprocessing(df_retired, df_non_retired)
+## Save
+preprocessed_data.to_csv(os.path.join(dir_str, 'preprocessed_data.csv'))
+pickle.dump(common_empty_columns, open(os.path.join(dir_str,'common_empty_columns.pkl'), 'wb'))
+
+
+## Normalization
+scaled_data, scaler = normalize_data(preprocessed_data)
+## Save
+scaled_data.to_csv(os.path.join(dir_str,'scaled_data.csv'))
+pickle.dump(scaler, open(os.path.join(dir_str,'scaler.pkl'), 'wb'))
+
+
+## PCA
+pca_data, pca_ = pca(scaled_data, 250)
+## Save
+pca_data.to_csv(os.path.join(dir_str,'pca_data.csv'))
+pickle.dump(pca_, open(os.path.join(dir_str,'pca_model.pkl'), 'wb'))
+
+##---------------------------------------------------------------------------------------------------------
+## ML SVM
+grid_param_svm = {'C':  [0.0001, 0.001,0.01, 0.1, 1, 10, 100],
+                  'gamma' : ['scale', 'auto', 0.0001, 0.001, 0.01, 0.1, 10, 100],
+                  'class_weight': ['balanced', None],
+                  'kernel' : ['sigmoid','poly','rbf']}
+
+## Perform 3 test-train splits
+
+TTS = [0.2,0.1,0.05] #Try reversing this to see if that does anything 
+for tts in TTS:
+
+  #TODO try renaming these best_svmTTS (without for loop)
+  print(tts*100)
+  best_svm, report_svm = svm_churn_tts(pca_data, grid_param_svm, tts)
+  #We need to clear the kernel for each training to avoid continuing 
+  print()
+  print()
+
+  #TTS for SVM
+
+## Creating the folder
+dir_str = 'Results_' + str(datetime.datetime.now())
+os.mkdir(dir_str)
+
+## Preprocessing
+preprocessed_data, common_empty_columns = preprocessing(df_retired, df_non_retired)
+## Save
+preprocessed_data.to_csv(os.path.join(dir_str, 'preprocessed_data.csv'))
+pickle.dump(common_empty_columns, open(os.path.join(dir_str,'common_empty_columns.pkl'), 'wb'))
+
+
+## Normalization
+scaled_data, scaler = normalize_data(preprocessed_data)
+## Save
+scaled_data.to_csv(os.path.join(dir_str,'scaled_data.csv'))
+pickle.dump(scaler, open(os.path.join(dir_str,'scaler.pkl'), 'wb'))
+
+
+## PCA
+pca_data, pca_ = pca(scaled_data, 250)
+## Save
+pca_data.to_csv(os.path.join(dir_str,'pca_data.csv'))
+pickle.dump(pca_, open(os.path.join(dir_str,'pca_model.pkl'), 'wb'))
+
+##---------------------------------------------------------------------------------------------------------
+## ML SVM
+grid_param_svm = {'C':  [0.0001, 0.001,0.01, 0.1, 1, 10, 100],
+                  'gamma' : ['scale', 'auto', 0.0001, 0.001, 0.01, 0.1, 10, 100],
+                  'class_weight': ['balanced', None],
+                  'kernel' : ['sigmoid','poly','rbf']}
+
+## Perform 3 test-train splits
+
+TTS = [0.2,0.1,0.05] #Try reversing this to see if that does anything 
+
+
+#TODO try renaming these best_svmTTS (without for loop)
+print("20% Test")
+best_svm20, report_svm20 = svm_churn_tts(pca_data, grid_param_svm, 0.2)
+print()
+
+print("10% Test")
+best_svm10, report_svm10 = svm_churn_tts(pca_data, grid_param_svm, 0.1)
+print()
+
+print("5% Test")
+best_svm5, report_svm5 = svm_churn_tts(pca_data, grid_param_svm, 0.05)
+
+## Save
+pickle.dump(best_svm, open(os.path.join(dir_str,'svm.sav'), 'wb'))
+pickle.dump(report_svm, open(os.path.join(dir_str,'svm_metrics.pkl'), 'wb'))
